7d88f8f5e1670d4d60a5ae0b5b2291c703e67e05
StacyWeru <stacyweru@gmail.com>
2020-09-29 12:21:36 +0300

    Modified quote-form.ts'

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -6,6 +6,7 @@ import { Quote} from '../quote';
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
+
 export class QuoteFormComponent implements OnInit {
 
   newQuote = new Quote(0,"","","",new Date());
