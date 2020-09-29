a893d0a07d3a3c877f083f1e54828d1c80cd6b85
StacyWeru <stacyweru@gmail.com>
2020-09-29 12:20:15 +0300

    Modified quote-details.ts

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -6,6 +6,7 @@ import { Quote } from '../quote';
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
+
 export class QuoteDetailsComponent implements OnInit {
   @Input() quote: Quote;
   @Output() isComplete = new EventEmitter<boolean>();
